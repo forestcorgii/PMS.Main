4942294737b80b4b86153c6d9bea17f4bbafbac8
Sean Ivan Fernandez_SW Programmer <seanivanf@gmail.com>
2022-07-27 14:30:16 +0800

    Added Select Download option UI.

---
+++ Pms.Main.FrontEnd.Wpf/Pages/Timesheet/SelectDownloadOption.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Pms.Timesheets.ServiceLayer.TimeSystem.Services;
+using static Pms.Timesheets.ServiceLayer.TimeSystem.Services.Enums;
+
+namespace Pms.Main.FrontEnd.Wpf
+{
+    /// <summary>
+    /// Interaction logic for SelectDownloadOption.xaml
+    /// </summary>
+    public partial class SelectDownloadOption : Window
+    {
+        public DownloadOptions DownloadOptions;
+
+        public SelectDownloadOption()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnDownloadAll_Click(object sender, RoutedEventArgs e)
+        {
+            DownloadOptions = DownloadOptions.All;
+        }
+
+        private void BtnDownloadUnconfirmed_Click(object sender, RoutedEventArgs e)
+        {
+            DownloadOptions = DownloadOptions.UnconfirmedOnly;
+        }
+    }
+}
